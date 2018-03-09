b282cdbc5f737255dff5f53c4a84e73fa149afeb
Konstantin Förster <kf@teuto.net>
2018-03-09 16:23:08 +0100

    Initial commit

---
+++ src/main/java/de/kgeorgiew/daocitemdb/DAoCItemDbApplication.java
@@ -0,0 +1,12 @@
+package de.kgeorgiew.daocitemdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DAoCItemDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DAoCItemDbApplication.class, args);
+	}
+}
